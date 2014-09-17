f0d214da7b4da9dfd0485caa25c9df68d963e3c1
kachinger <kris@achinger.pl>
2014-09-16 23:34:32 -0700

    Fixing Eli's 'Send to yourself silly form'

--- email_script.php
+++ email_script.php
@@ -1,12 +1,11 @@
 <?
 
-$from="webmail@achinger.pl";
 $email=$_POST['contactEmail'];
 $subject=$_POST['contactSubject'];
 $message=$_POST['contactMessage'];
 
-mail("achingerpl@gmail.com", $email, $subject, $message);
+mail("achingerpl@gmail.com",$subject,$message,"From: $email\n");
 
-print "Your message has been sent: </br>$email</br>$subject</br>$message";
+print "Your message has been sent: </br>$subject</br>$message";
 
 ?>
