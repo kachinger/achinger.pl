2ef4ea0441491ecc695863efa6f91c366bb35a96
kachinger <kris@achinger.pl>
2014-09-16 23:00:41 -0700

    Adding email form and modyfying Submit button in index.html

---
+++ email_script.php
@@ -0,0 +1,12 @@
+<?
+
+$from="webmail@achinger.pl";
+$email=$_POST['contactEmail'];
+$subject=$_POST['contactSubject'];
+$message=$_POST['contactMessage'];
+
+mail($email, $subject, $message, "From: ",$from);
+
+print "Your message has been sent: </br>$email</br>$subject</br>$message;
+
+>?
