cce84ef8b4ba0be2866227ddbda32d78e661c9da
kachinger <kris@achinger.pl>
2014-09-16 23:24:52 -0700

    Fixing Warning: mail() expects at most 5 parameters, 6 given in /home/achinger/webapps/achinger/email_script.php on line 8

--- email_script.php
+++ email_script.php
@@ -5,7 +5,7 @@ $email=$_POST['contactEmail'];
 $subject=$_POST['contactSubject'];
 $message=$_POST['contactMessage'];
 
-mail("achingerpl@gmail.com", $email, $subject, $message, "From: ",$from);
+mail("achingerpl@gmail.com", $email, $subject, $message);
 
 print "Your message has been sent: </br>$email</br>$subject</br>$message";
 
