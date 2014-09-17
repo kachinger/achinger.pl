6c2ef84c296236259d976843faacdbcfb39de0af
kachinger <kris@achinger.pl>
2014-09-16 23:21:35 -0700

    Adding To: email address

--- email_script.php
+++ email_script.php
@@ -5,7 +5,7 @@ $email=$_POST['contactEmail'];
 $subject=$_POST['contactSubject'];
 $message=$_POST['contactMessage'];
 
-mail($email, $subject, $message, "From: ",$from);
+mail("achingerpl@gmail.com", $email, $subject, $message, "From: ",$from);
 
 print "Your message has been sent: </br>$email</br>$subject</br>$message";
 
